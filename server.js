934aa57ff63b30519c4a9434a370d121165f0547
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-01 09:56:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 const routes = require('./routes');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,  
+}));
+app.use(cookieParser());
 app.use(express.json());
 
 const dbConnect = require('./config/dbConnect');
@@ -15,8 +20,7 @@ app.get('/',(req,res)=>{
   res.send('eCommerce wesite is running....');
 });
 
-app.use('/api',routes);
-
+app.use('/api',routes); 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
