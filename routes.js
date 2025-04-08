29d915362fdd6015b201856ca6ddca2f51be6244
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-08 15:13:17 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
+const adminController = require('./controllers/adminController');
 const auth = require('./middlewares/auth');
+const uploadImage = require('./middlewares/uploadImage');
 
+// Auth Routes
 router.post('/user-register',userController.register);
 router.post('/user-login',userController.login);
 router.get('/verify-user', auth,userController.verifyUser);
 router.post('/logout', auth,userController.logout);
+
+//Admin Routes 
+router.post('/admin/upload/products', uploadImage.single('file'), adminController.uploadProduct);
 
 router.get('/home',auth,(req,res)=>{
   const user = req.user;
